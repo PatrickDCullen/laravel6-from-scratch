1b5047f2720e42c05001104cfd4062d0b7e86f76
PatrickDCullen <cullenpatrickd@gmail.com>
2021-02-25 20:36:03 -0500

    End Ep 22: Restful Routing

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,14 @@
 <?php
 
-use App\Models\Article;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostsController;
 use App\Http\Controllers\ArticlesController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function() {
-    return view('welcome');
-});
-
-Route::get('/about', function() {
-    return view('about', [
-        'articles' => Article::take(3)->latest()->get()
-    ]);
-});
 
 Route::get('/articles', [ArticlesController::class, 'index']);
 Route::get('/articles/{article}', [ArticlesController::class, 'show']);
-Route::get('/articles/{article}/edit', [ArticlesController::class, 'edit']);
+
+// GET /articles
+// GET /articles/:id
+// POST /articles
+// PUT /articles/:id
+// DELETE /articles/:id
+
