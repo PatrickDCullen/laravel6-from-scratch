f007e8fee6cb0ccd0ba249f6f05a10b8b65c421b
PatrickDCullen <cullenpatrickd@gmail.com>
2021-02-26 14:30:22 -0500

    End Ep 25: From Validation Essentials

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -16,7 +16,16 @@
                     <label class="label" for="title">Title</label>
 
                     <div class="control">
-                        <input type="text" class="input" name="title" id="title">
+                        <input
+                            type="text"
+                            class="input @error('title') is-danger @enderror"
+                            name="title"
+                            id="title"
+                            value="{{ old('title') }}">
+
+                        @error('title')
+                            <p class="help is-danger">{{ $errors->first('title') }}</p>
+                        @enderror
                     </div>
                 </div>
 
@@ -25,7 +34,16 @@
                     <label for="excerpt" class="label">Excerpt</label>
 
                     <div class="control">
-                        <textarea name="excerpt" id="excerpt" class="textarea"></textarea>
+                        <textarea
+                            name="excerpt"
+                            id="excerpt"
+                            class="textarea @error('excerpt') is-danger @enderror"
+                            value="{{ old('excerpt') }}">
+                        </textarea>
+
+                        @error('excerpt')
+                            <p class="help is-danger">{{ $errors->first('excerpt') }}</p>
+                        @enderror
                     </div>
                 </div>
 
@@ -33,7 +51,16 @@
                     <label for="body" class="label">Body</label>
 
                     <div class="control">
-                        <textarea name="body" id="body" class="textarea"></textarea>
+                        <textarea
+                            name="body"
+                            id="body"
+                            class="textarea @error('body') is-danger @enderror"
+                            value="{{ old('body') }}">
+                        </textarea>
+
+                        @error('body')
+                            <p class="help is-danger">{{ $errors->first('body') }}</p>
+                        @enderror
                     </div>
                 </div>
 
