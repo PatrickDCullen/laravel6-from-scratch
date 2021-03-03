761260538dfa7f9789761f069762c0c8e91d014f
PatrickDCullen <cullenpatrickd@gmail.com>
2021-03-03 16:07:12 -0500

    End Ep 46: Notifications versus Mailables (also added auth scaffolding from eps 34 and 35

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,28 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ArticlesController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ArticlesController;
+use App\Http\Controllers\PaymentsController;
 
 Route::get('/', function() {
     return view('welcome');
 });
 
-Route::get('/contact', [ContactController::class, 'show']);
-Route::post('/contact', [ContactController::class, 'store']);
+Route::get('/payments/create', [PaymentsController::class, 'create'])->middleware('auth');
+Route::post('/payments', [PaymentsController::class, 'store'])->middleware('auth');
 
-Route::get('/articles', [ArticlesController::class, 'index'])->name('articles.index');
-Route::post('/articles', [ArticlesController::class, 'store']);
-Route::get('/articles/create', [ArticlesController::class, 'create']);
-Route::get('/articles/{article}', [ArticlesController::class, 'show'])->name('articles.show');
-Route::get('/articles/{article}/edit', [ArticlesController::class, 'edit']);
-Route::put('/articles/{article}', [ArticlesController::class, 'update']);
+Auth::routes();
+
+// Route::get('/contact', [ContactController::class, 'show']);
+// Route::post('/contact', [ContactController::class, 'store']);
+
+// Route::get('/articles', [ArticlesController::class, 'index'])->name('articles.index');
+// Route::post('/articles', [ArticlesController::class, 'store']);
+// Route::get('/articles/create', [ArticlesController::class, 'create']);
+// Route::get('/articles/{article}', [ArticlesController::class, 'show'])->name('articles.show');
+// Route::get('/articles/{article}/edit', [ArticlesController::class, 'edit']);
+// Route::put('/articles/{article}', [ArticlesController::class, 'update']);
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
